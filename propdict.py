ca0c498e554d88893e62f214be7162cabb48105e
Tom Lazar <tomster@pyfidelity.com>
2013-03-10 12:04:01 +0100

    fix attribute access

--- propdict.py
+++ propdict.py
@@ -3,7 +3,6 @@ class propdict(dict):
     def __init__(self, **config):
         for key, value in config.pop('host').items():
             self[key] = value
-            print "setting %s to %s" % (key, value)
 
     def __getitem__(self, key):
         if key in self.keys():
@@ -12,5 +11,7 @@ class propdict(dict):
             return getattr(self, key)
 
     def __getattribute__(self, name):
-        # Default behaviour
-        return object.__getattribute__(self, name)
+        try:
+            return dict.__getitem__(self, name)
+        except KeyError:
+            return object.__getattribute__(self, name)
